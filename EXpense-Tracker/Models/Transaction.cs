143b62b89b746a7a2138ae30814ac8f453752ae9
Debankur-embee <debankur.b@embee.co.in>
2022-11-22 21:16:33 +0530

    Initial Commit ob base structure

---
+++ EXpense-Tracker/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EXpense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public int CategoryId { get; set; }
+        // to define there is a foreign key concept associated with categoryId
+        public Category Category { get; set; } 
+        public int Amount { get; set; }
+        [Column(TypeName ="nvarchar(150)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
